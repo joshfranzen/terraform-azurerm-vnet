57ed96b8f7fedfe2d071bb29d85a26e5a3359ba6
joshfranzen <38957034+joshfranzen@users.noreply.github.com>
2021-09-16 15:16:17 -0500

    update subnet prefix lookup
    changed array of count.index to element lookup since it's a list

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource azurerm_virtual_network "vnet" {
 
 resource "azurerm_subnet" "subnet" {
   count                                          = length(var.subnet_names)
-  name                                           = var.subnet_names[count.index]
+  name                                           = element(var.subnet_names,[count.index])
   resource_group_name                            = data.azurerm_resource_group.vnet.name
   virtual_network_name                           = azurerm_virtual_network.vnet.name
   address_prefixes                               = [var.subnet_prefixes[count.index]]
